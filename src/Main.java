28f6ddd710a94033a001a3c0c3704d61abf781c2
mahmoodalgbory <algbormd@mail.uc.edu>
2022-09-22 14:32:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+    public static void main(String[] args
+    {
+       int birthMonth = 2;
+
+       if (birthMonth >= 1 & <= 12)
+
+
+           System.out.println("your birth month is " + birthMonth);
+
+       else
+           System.out.println("you entered an invalid number");
+    }
+}
+
+
+//Class birthMonth
+//  main()
+//    num month = 0
+//
+//    Output "Enter your birth month. "
+//    input birthMonth
+//
+//    if birthMonth >= 1 and <= 12
+//    	output "Your birth month is: " + birthMonth
+//    else
+//    	Output "You entered an incorrect month value. "
+//    endIf
+//
+//   return
+//endClass
